61a9db3853bd2ed7e845e712388f948bf853f272
GiovanniTenaglia <86293429+GiovanniTenaglia@users.noreply.github.com>
2022-03-04 12:17:09 +0100

    Update split-join.js

--- split_join/js/split-join.js
+++ split_join/js/split-join.js
@@ -25,7 +25,7 @@ var initialize = true;
 var counter = 0;
 function version()
 {
-	window.alert("prova 12");
+	window.alert("prova 13");
 	initialize=false;
 }
 
@@ -316,7 +316,7 @@ $(async function() {
 			{
 				var txt = extractContent(result.data[i].values[RM.Data.Attributes.PRIMARY_TEXT]).replace('\xA0',' ');
 				var htmltxt = result.data[i].values[RM.Data.Attributes.PRIMARY_TEXT];
-				if(i<17) window.alert(result.data[i].values[RM.Data.Attributes.FORMAT]);
+				if(i<17) window.alert(result.data[i].values[RM.Data.Attributes.FORMAT] + ": \n" + htmltxt);
 				if((txt.startsWith("Tabella ") || txt.startsWith("Figura ")) && !(htmltxt.includes("<table ") || htmltxt.includes("<img ")) && !(result.data[i].values[RM.Data.Attributes.ARTIFACT_TYPE].name.includes("Intestazione")))
 				{
 					var ii = i-1;
