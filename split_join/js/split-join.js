83ab12c8bceb36913731ad01f1522a47c6b6c665
GiovanniTenaglia <86293429+GiovanniTenaglia@users.noreply.github.com>
2022-03-08 23:52:57 +0100

    Update split-join.js

--- split_join/js/split-join.js
+++ split_join/js/split-join.js
@@ -329,10 +329,10 @@ $(async function() {
 					total++;
 					var ii = i-1;
 					while(!result.data[ii].values[RM.Data.Attributes.PRIMARY_TEXT].includes("<table ")) ii--;
-					window.alert("Tabella " + txt.replace( /(^.+\D)(\d+)(\D.+$)/i,'$2'));
+					window.alert("Tabella " + txt.match(/\d+/).shift());
 					window.alert(result.data[ii-1].values[RM.Data.Attributes.PRIMARY_TEXT]);
-					window.alert((result.data[ii-1].values[RM.Data.Attributes.PRIMARY_TEXT].includes("Tabella " + txt.replace( /(^.+\D)(\d+)(\D.+$)/i,'$2'))));
-					if(result.data[ii-1].values[RM.Data.Attributes.PRIMARY_TEXT].includes("Tabella " + txt.replace( /(^.+\D)(\d+)(\D.+$)/i,'$2'))) captionpairs.push(result.data[ii-1].ref, result.data[ii].ref,result.data[i].ref);
+					window.alert((result.data[ii-1].values[RM.Data.Attributes.PRIMARY_TEXT].includes("Tabella " + txt.match(/\d+/).shift())));
+					if(result.data[ii-1].values[RM.Data.Attributes.PRIMARY_TEXT].includes("Tabella " + txt.match(/\d+/).shift())) captionpairs.push(result.data[ii-1].ref, result.data[ii].ref,result.data[i].ref);
 					else captionpairs.push(null,result.data[ii].ref,result.data[i].ref);
 				}
 			}
