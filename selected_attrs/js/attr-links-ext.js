37d2295aba163c75369ed095bbed418a4e2e28c9
GiovanniTenaglia <86293429+GiovanniTenaglia@users.noreply.github.com>
2022-10-28 10:09:07 +0200

    Update attr-links-ext.js

--- selected_attrs/js/attr-links-ext.js
+++ selected_attrs/js/attr-links-ext.js
@@ -73,8 +73,8 @@ function fetchAttributes(/*RM.ArtifactRef*/ attrRef) {
 				getValue(table, valResult.data[i], attrs, name);
 			}
 			addTableRows(["<b>URI", attrRef.toUri()], table, ["", ""]);
-                        addTableRows(["<b>HTML content", "<plaintext>" + extractContent(attrResult.data[0].values[RM.Data.Attributes.PRIMARY_TEXT]).replace(/&/g,"&amp;").replace(/</g,"&lt;").replace(/>/g,"&gt;")], table, ["", ""]);
-			window.alert(extractContent(attrResult.data[0].values[RM.Data.Attributes.PRIMARY_TEXT]));
+                        addTableRows(["<b>HTML content", "<plaintext>" + attrResult.data[0].values[RM.Data.Attributes.PRIMARY_TEXT].replace(/&/g,"&amp;").replace(/</g,"&lt;").replace(/>/g,"&gt;")], table, ["", ""]);
+			window.alert(attrResult.data[0].values[RM.Data.Attributes.PRIMARY_TEXT]);
 
 			// Add the table we have constructed to the attributes section of the gadget.
 			$("#attributes").append(table);
