62b79ebea6036eac4e9fd47751b2d96432049468
GiovanniTenaglia <86293429+GiovanniTenaglia@users.noreply.github.com>
2023-06-20 11:19:05 +0200

    Update split-join.js

--- split_join/js/split-join.js
+++ split_join/js/split-join.js
@@ -337,12 +337,6 @@ $(async function() {
 				}
 				var txt = extractContent(result.data[i].values[RM.Data.Attributes.PRIMARY_TEXT]).replace(/\n/g,' ').replace(/\xA0/g,' ').trim();
 				var htmltxt = result.data[i].values[RM.Data.Attributes.PRIMARY_TEXT];
-				/*if((txt.startsWith("Tabella ") || txt.startsWith("Figura ")) && !(htmltxt.includes("<table ") || htmltxt.includes("<img ")) && !(result.data[i].values[RM.Data.Attributes.ARTIFACT_TYPE].name.includes("Intestazione")))
-				{
-					var ii = i-1;
-					while(!result.data[ii].values[RM.Data.Attributes.PRIMARY_TEXT].includes("<img ") && !result.data[ii].values[RM.Data.Attributes.PRIMARY_TEXT].includes("<table ")) ii--;
-					captionpairs.push(result.data[ii].ref,result.data[i].ref);
-				}*/
 				if(txt.startsWith("Tabella ") && (htmltxt.includes("<b>Tabella") || htmltxt.includes("<b><span>Tabella")) && !htmltxt.includes("<table ") && !(result.data[i].values[RM.Data.Attributes.ARTIFACT_TYPE].name.includes("Intestazione")))
 				{
 					total++;
